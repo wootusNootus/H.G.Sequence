f6ef905ca5075951bc0140082f5281355bd570c9
wootusNootus <migospodinoff@gmail.com>
2025-02-09 21:48:43 -0600

    Runtime func

--- H.G.Sequence/src/App.java
+++ H.G.Sequence/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        printNGNumbers(10);
+        System.out.println("Testing run time for recursion:");
+        testRuntime(() -> G(320));
     }
 
     public static int G(int n){
@@ -19,6 +20,21 @@ public class App {
         for(int i = 0; i <= n; i++){
             System.out.println("Term " + (i+1) + ": " + G(i));
         }
+    }
+
+    // Used the following website for tutorial
+    // https://www.tutorialspoint.com/how-to-implement-the-runnable-interface-using-lambda-expression-in-java
+    public static void testRuntime(Runnable function) {
+        // Testing runtime for x
+        long startTime = System.nanoTime();
+
+        // Running the function
+        function.run();
+
+        long endTime = System.nanoTime();
+
+        double duration = (endTime - startTime)/1_000_000.0;
+        System.out.println("Execution time: "+ duration + "ms");
     }
 }
 
